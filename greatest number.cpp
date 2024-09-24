d1ac38a7438b036be29c1f48672aed247dc28c8a
Ashish Chavanke <datta.ashish24@sanjivani.edu.in>
2024-09-24 23:40:29 +0530

    Add files via upload

---
+++ greatest number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int a = 2, b = 9;
+
+// Conditional Operator
+int result = (a < b) ? b : a;
+cout << "The greatest number is " << result << endl;
+
+return 0;
+}
